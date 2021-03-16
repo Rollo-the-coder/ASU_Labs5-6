1f5663c8dd31418ed0d1ba9c4f5cba8728c7b312
Rollo-the-coder <erikgotta@gmail.com>
2021-03-17 05:35:10 +0700

    Cleaned up Lab6 and removed 1 from being counted as prime Palindrome number

--- Lab6.java/src/Lab6.java
+++ Lab6.java/src/Lab6.java
@@ -1,30 +1,37 @@
+//========================================
+//Lab6.java
+//Name : Erik Christian Gotta
+//ASU ID : 1222628953
+//Time taken to complete this lab: 60
+//========================================
+
 class Lab6 {
     public static void main(String[] args) {
         int primePalindromes = 0;
-		int number = 1;
+		int number;
         
-        //System.out.println("Please enter a number to check if its a prime Palindrome! ");
-
-        for (number = 1; number <= 100000; number++) {
+        // for loop to check 1 - 100,000 for prime palindrome numbers. Starts at 2 since one is not a prime palindrome
+        for (number = 2; number <= 100000; number++) {
 			if (isPrime(number) == true && isPalindrome(number) == true) {
 				primePalindromes++;
+                //System.out.print(number + ",");
 			}
 		}
-        System.out.println("There is " + primePalindromes + " prime Palindromes in the number 100,000");
+        System.out.println("There is " + primePalindromes + " prime Palindromes in the total of 100,000");
     }
 
+    //function to check if prime
     static boolean isPrime(int num) {
         int i;
-        for (i = 2; i <= (num/2); i++) 
-        {
-            if (num % i == 0) 
-            {
+        for (i = 2; i <= (num/2); i++) {
+            if (num % i == 0) {
                 return false;
             }            
         }
         return true;
     }
 
+    //function to check if Palindrome number
     static boolean isPalindrome(int num) {
         int copy = num;
         int reversed = 0;
@@ -41,7 +48,5 @@ class Lab6 {
         } else {
             return false;
         }
-
-
     }
 }
